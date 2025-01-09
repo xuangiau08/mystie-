5421d98561b90bfcf3454156ed0e93b6b1f1e24d
xuangiau08 <xuangiau08@gmail.com>
2025-01-09 22:22:26 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  resources :products
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "/products", to: "products#index"
+
+  get "/products/new", to: "products#new"
+  post "/products", to: "products#create"
+  
+  get "/products/:id", to: "products#show"
+  
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
+  
+  delete "/products/:id", to: "products#destroy"
+  
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  root "products#index"
+
+end
